830defaa1c64130c5756ddc54854f49cd45bf642
Ritage hamada <ritaj.hamada.9@gmail.com>
2025-09-16 19:15:43 -0700

    Fix vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: '/ritage2.github.io/',
+  base: "./",
 })
